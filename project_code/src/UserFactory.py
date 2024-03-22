3194f6f731d84e91d41a030a61c197e88e21b4c9
lgao5 <150876110+lgao5@users.noreply.github.com>
2024-03-22 01:21:05 +0000

    added csv reader to store users in UserFactory, as well as modified get_user_by_username method and added save_user_to_csv method

--- project_code/src/UserFactory.py
+++ project_code/src/UserFactory.py
@@ -1,22 +1,40 @@
 # UserFactory.py
 from project_code.src.UserInputParser import UserInputParser
 from project_code.src.User import User
+import csv
 
 
 class UserFactory:
-
-    #store a list of all users
-    list_of_users = {}
     
     @staticmethod
     def create_user(parser: UserInputParser) -> User:
         #creates user instance
         username = parser.parse("Enter a username: ")
         password = parser.parse("Enter a password: ")
+        user = User(username, password)
 
-        #adds user instance to list of users
-        UserFactory.list_of_users[username] = User(username, password)
-        return User(username, password)
+        #save user data to CSV file
+        UserFactory.save_user_to_csv(user, "users.csv")
+
+        return user
     
-    def get_user_by_username(username: str) -> User | None:
-        return UserFactory.list_of_users.get(username)
+    def save_user_to_csv(user:User, filename: str):
+        #convert user instance to dict
+        user_data = user.to_dict()
+
+        #write user data to CSV file
+        with open(filename, 'a', newline='') as csvfile:
+            fieldnames = user_data.keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            #write data
+            writer.writerow(user_data)
+    
+    def get_user_by_username(username: str, filename: str) -> User | None:
+        #read user data from CSV file and search for username
+        with open(filename, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['username'] == username:
+                    return User(row['username'], row['password'])
+        return None
